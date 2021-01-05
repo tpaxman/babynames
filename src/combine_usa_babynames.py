ac88d47669944a59d36fef13f8c75826689b8920
Tyler Paxman <tylerpaxman@gmail.com>
2021-01-05 10:08:54 -0700

    finalize required scripts for usa baby names with percents

--- src/combine_usa_babynames.py
+++ src/combine_usa_babynames.py
@@ -1,16 +1,37 @@
 import pandas as pd
 from pathlib import Path
 
-df_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('*.csv')])
 
-df_m = (df_raw[['Rank', 'Male name', 'Number of males']]
-        .rename(columns={'Rank': 'rank', 'Male name': 'babyname', 'Number of males': 'number'})
-        .assign(gender='Male'))
+# get number data
+df_num_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('number*.csv')])
 
-df_f = (df_raw[['Rank', 'Female name', 'Number of females']]
-        .rename(columns={'Rank': 'rank','Female name': 'babyname','Number of females': 'number'})
-        .assign(gender='Female'))
+df_num_m = (df_num_raw[['Rank', 'Male name', 'Number of males', 'year']]
+            .rename(columns={'Rank': 'rank', 'Male name': 'babyname', 'Number of males': 'number'})
+            .assign(gender='Male'))
 
-df = pd.concat([df_m, df_f])
+df_num_f = (df_num_raw[['Rank', 'Female name', 'Number of females', 'year']]
+            .rename(columns={'Rank': 'rank','Female name': 'babyname','Number of females': 'number'})
+            .assign(gender='Female'))
+
+df_num = pd.concat([df_num_m, df_num_f])
+
+
+# get percent data
+df_pct_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('percent*.csv')])
+
+df_pct_m = (df_pct_raw[['Rank', 'Male name', 'Percent oftotal males', 'year']]
+            .rename(columns={'Rank': 'rank', 'Male name': 'babyname', 'Percent oftotal males': 'percent'})
+            .assign(gender='Male'))
+
+df_pct_f = (df_pct_raw[['Rank', 'Female name', 'Percent oftotal females', 'year']]
+            .rename(columns={'Rank': 'rank','Female name': 'babyname','Percent oftotal females': 'percent'})
+            .assign(gender='Female'))
+
+df_pct = pd.concat([df_pct_m, df_pct_f])
+
+
+# combine number and percent data
+df = pd.merge(df_num, df_pct, on=['rank', 'babyname', 'gender', 'year'])
+
 
 df.to_csv('usa-babynames.csv', index=False)
