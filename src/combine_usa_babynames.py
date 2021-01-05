9dcf5b28fe5bced215de268086629ab440ce773d
Tyler Paxman <tylerpaxman@gmail.com>
2021-01-05 10:31:27 -0700

    make combine_usa_babynames more concise

--- src/combine_usa_babynames.py
+++ src/combine_usa_babynames.py
@@ -1,37 +1,31 @@
 import pandas as pd
 from pathlib import Path
 
+HEADERS = {
+    'Rank': 'rank',
+    'Male name': 'babyname',
+    'Female name': 'babyname',
+    'Number of males': 'number',
+    'Number of females': 'number',
+    'Percent oftotal males': 'percent',
+    'Percent oftotal females': 'percent'
+}
 
 # get number data
-df_num_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('number*.csv')])
-
-df_num_m = (df_num_raw[['Rank', 'Male name', 'Number of males', 'year']]
-            .rename(columns={'Rank': 'rank', 'Male name': 'babyname', 'Number of males': 'number'})
-            .assign(gender='Male'))
-
-df_num_f = (df_num_raw[['Rank', 'Female name', 'Number of females', 'year']]
-            .rename(columns={'Rank': 'rank','Female name': 'babyname','Number of females': 'number'})
-            .assign(gender='Female'))
-
-df_num = pd.concat([df_num_m, df_num_f])
-
+num_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('number*.csv')])
+num_m = num_raw[['Rank', 'Male name', 'Number of males', 'year']].rename(columns=HEADERS).assign(sex='Male')
+num_f = num_raw[['Rank', 'Female name', 'Number of females', 'year']].rename(columns=HEADERS).assign(sex='Female')
+df_num = pd.concat([num_m, num_f])
 
 # get percent data
-df_pct_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('percent*.csv')])
-
-df_pct_m = (df_pct_raw[['Rank', 'Male name', 'Percent oftotal males', 'year']]
-            .rename(columns={'Rank': 'rank', 'Male name': 'babyname', 'Percent oftotal males': 'percent'})
-            .assign(gender='Male'))
-
-df_pct_f = (df_pct_raw[['Rank', 'Female name', 'Percent oftotal females', 'year']]
-            .rename(columns={'Rank': 'rank','Female name': 'babyname','Percent oftotal females': 'percent'})
-            .assign(gender='Female'))
-
-df_pct = pd.concat([df_pct_m, df_pct_f])
-
+pct_raw = pd.concat([pd.read_csv(str(f)) for f in Path('output').glob('percent*.csv')])
+pct_m = pct_raw[['Rank', 'Male name', 'Percent oftotal males', 'year']].rename(columns=HEADERS).assign(sex='Male')
+pct_f = pct_raw[['Rank', 'Female name', 'Percent oftotal females', 'year']].rename(columns=HEADERS).assign(sex='Female')
+df_pct = pd.concat([pct_m, pct_f])
 
 # combine number and percent data
-df = pd.merge(df_num, df_pct, on=['rank', 'babyname', 'gender', 'year'])
-
+df = pd.merge(df_num, df_pct, on=['rank', 'babyname', 'sex', 'year'])
+df['fraction'] = df['percent'].str[:-1].astype(float) / 100
+df = df[['year', 'sex', 'rank', 'babyname', 'number', 'fraction']]
 
 df.to_csv('usa-babynames.csv', index=False)
